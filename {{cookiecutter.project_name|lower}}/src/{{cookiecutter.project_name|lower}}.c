391047c9f7715bd63ce58fb9ddce07e6b192c8dc
Vincent Bernat <bernat@luffy.cx>
2014-02-03 22:19:09 +0100

    Use libargtable2 instead of jansson as an example lib.

--- {{cookiecutter.project_name|lower}}/src/{{cookiecutter.project_name|lower}}.c
+++ {{cookiecutter.project_name|lower}}/src/{{cookiecutter.project_name|lower}}.c
@@ -22,59 +22,65 @@
 
 #include <stdio.h>
 #include <unistd.h>
-#include <getopt.h>
+#include <argtable2.h>
 #include <string.h>
 
 extern const char *__progname;
 
-static void
-usage(void)
-{
-	/* TODO:3002 Don't forget to update the usage block with the most
-	 * TODO:3002 important options. */
-	fprintf(stderr, "Usage: %s [OPTIONS]\n",
-		__progname);
-	fprintf(stderr, "Version: %s\n", PACKAGE_STRING);
-	fprintf(stderr, "\n");
-	fprintf(stderr, "-d      Be more verbose.\n");
-	fprintf(stderr, "\n");
-	fprintf(stderr, "see manual page " PACKAGE "(8) for more information\n");
-}
-
 int
-main(int argc, char * const argv[])
+main(int argc, char *argv[])
 {
-	int debug = 1;
-	int ch;
+	int exitcode = EXIT_FAILURE;
 
 	/* TODO:3001 If you want to add more options, add them here. */
+	struct arg_end *arg_fini    = arg_end(5);
+	struct arg_lit *arg_debug   = arg_litn("d", "debug", 0, 3, "be more verbose");
+	struct arg_lit *arg_help    = arg_lit0("h", "help", "display help and exit");
+	struct arg_lit *arg_version = arg_lit0("v", "version", "print version and exit");
+	struct arg_str *arg_filter  = arg_strn("D", NULL, NULL, 0, 10, "set allowed debug tokens");
+	void *argtable[] = { arg_debug,
+			     arg_help,
+			     arg_version,
+			     arg_filter,
+			     /* Other arguments should be here */
+			     arg_fini
+	};
 
-	while ((ch = getopt(argc, argv, "hvdD:")) != -1) {
-		switch (ch) {
-		case 'h':
-			usage();
-			exit(0);
-			break;
-		case 'v':
-			fprintf(stdout, "%s\n", PACKAGE_VERSION);
-			exit(0);
-			break;
-		case 'd':
-			debug++;
-			break;
-		case 'D':
-			log_accept(optarg);
-			break;
-		default:
-			usage();
-			exit(1);
-		}
+	if (arg_nullcheck(argtable) != 0) {
+		fprintf(stderr, "%s: insufficient memory\n", __progname);
+		goto exit;
 	}
 
-	log_init(debug, __progname);
+	int n = arg_parse(argc, argv, argtable);
+	if (n != 0 || arg_help->count) {
+		if (n != 0)  arg_print_errors(stderr, arg_fini, __progname);
+		else exitcode = EXIT_SUCCESS;
+		fprintf(stderr, "Usage: %s", __progname);
+		arg_print_syntax(stderr, argtable, "\n");
+		arg_print_glossary(stderr, argtable, "  %-25s %s\n");
+		fprintf(stderr, "\n");
+		fprintf(stderr, "see manual page " PACKAGE "(8) for more information\n");
+		goto exit;
+	}
+
+	if (arg_version->count) {
+		fprintf(stdout, "%s\n", PACKAGE_VERSION);
+		exitcode = EXIT_SUCCESS;
+		goto exit;
+	}
+
+	for (int i = 0; i < arg_filter->count; i++)
+		log_accept(arg_filter->sval[i]);
+
+	log_init(arg_debug->count, __progname);
 
 	/* TODO:3000 It's time for you program to do something. Add anything
 	 * TODO:3000 you want here. */
 	log_info("main", "hello world!");
 	log_warnx("main", "your program does nothing");
+
+	exitcode = EXIT_SUCCESS;
+exit:
+	arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
+	return exitcode;
 }
